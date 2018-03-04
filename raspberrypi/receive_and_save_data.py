874e3f3829557fc55e3a39a7f66d4799f78e610f
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-03-04 19:20:22 +0200

    Write sensor data to InfluxDB

--- raspberrypi/receive_and_save_data.py
+++ raspberrypi/receive_and_save_data.py
@@ -1,17 +1,29 @@
-from pymongo import MongoClient
-import pprint, datetime, serial, json
+from influxdb import InfluxDBClient
+import datetime, serial, json
 
-client = MongoClient('mongodb://192.168.0.104:27017/')
-db = client.sensor_data
+client = InfluxDBClient('192.168.0.104', 8086, 'root', 'root', 'plants')
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
 while 1:
     try:
         response = ser.readline().decode('utf-8').replace("\x00\r\n", "")
         print(str(datetime.datetime.now()) + " : response: " + response)
-        parsed_response = json.loads(response);
-        sensor_data_entry = parsed_response.copy();
-        sensor_data_entry.date = datetime.datetime.now();
-        sensor_data_id = db.sensor_data.insert_one(sensor_data_entry).inserted_id
+        parsed_response = json.loads(response)
+        data = [{
+            "measurement": "plant",
+            "tags": {
+                "nodeid": parsed_response["nodeid"]
+            },
+            "time": datetime.datetime.now(),
+            "fields": {
+                "moisture": parsed_response["moisture"],
+                "moisture_precentage": float(parsed_response["moisture_precentage"]),
+                "m_dry": parsed_response["m_dry"],
+                "m_wet": parsed_response["m_wet"],
+                "temperature": parsed_response["temperature"],
+                "light": parsed_response["light"]
+            }
+        }]
+        client.write_points(data)
     except Exception as e:
         print("Error: ", e)
