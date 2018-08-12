435a57f0abd9ebe4efef52a6494d6e5a86151726
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-08-12 12:32:14 +0300

    Add cors headers in response

--- graph/functions/src/handler.js
+++ graph/functions/src/handler.js
@@ -1,17 +1,35 @@
 const SensorService = require('./SensorService');
 
+const ALLOWED_ORIGINS = [
+  'http://localhost:3000',
+  'http://cleverhome.link',
+  'https://cleverhome.link',
+];
+
+const getCorsHeaders = (origin) => {
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    return {
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Credentials': true,
+    };
+  }
+  return {};
+};
+
 const handlePostReading = async (event, context, callback) => {
   console.log(JSON.stringify(event));
+  const headers = getCorsHeaders(event.headers.origin);
   const parsedBody = JSON.parse(event.body);
   await SensorService.saveReading(parsedBody);
-  callback(null, { statusCode: 200, body: JSON.stringify({ message: 'success' }) });
+  callback(null, { statusCode: 200, headers, body: JSON.stringify({ message: 'success' }) });
 };
 
 const handleGetReadings = async (event, context, callback) => {
   console.log(JSON.stringify(event));
+  const headers = getCorsHeaders(event.headers.origin);
   const { nodeid, limit } = event.queryStringParameters;
   const result = await SensorService.getReadings(nodeid, limit);
-  callback(null, { statusCode: 200, body: JSON.stringify(result) });
+  callback(null, { statusCode: 200, headers, body: JSON.stringify(result) });
 };
 
 module.exports = {
