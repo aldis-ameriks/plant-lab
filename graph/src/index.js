81247f7199dc667271cb66ffa265d71cbfa14b38
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-10-07 13:59:25 +0300

    Remove service worker

--- graph/src/index.js
+++ graph/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './utils/registerServiceWorker';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
