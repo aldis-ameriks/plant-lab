06381f9e88f9c65e3382eb209d6ac8d83f1957c5
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-10-14 15:21:53 +0300

    Remove unrelated fields

--- data-visualizer-graphql/index.js
+++ data-visualizer-graphql/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { find, filter } = require('lodash');
 const Influx = require('influx');
 const { GraphQLDateTime } = require('graphql-iso-date');
 
@@ -41,32 +40,8 @@ const saveReading = async reading => {
 const getReadings = (nodeid = 99, limit = 100) =>
   client.query(`select * from plant where nodeid=${nodeid} order by time desc limit ${limit}`);
 
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-    foo: 'bar'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
-];
-
-const authors = [{ name: 'J.K. Rowling' }, { name: 'Michael Crichton' }];
-
 const typeDefs = gql`
   scalar DateTime
-  type Book {
-    title: String
-    author: Author
-  }
-
-  type Author {
-    name: String
-    books: [Book]
-  }
-
   type Reading {
     time: DateTime
     m_dry: Int
@@ -78,8 +53,6 @@ const typeDefs = gql`
   }
 
   type Query {
-    books: [Book]
-    authors: [Author]
     readings: [Reading]
   }
 `;
@@ -87,16 +60,8 @@ const typeDefs = gql`
 const resolvers = {
   DateTime: GraphQLDateTime,
   Query: {
-    books: () => books,
-    authors: () => authors,
     readings: () => getReadings()
   },
-  Author: {
-    books: author => filter(books, { author: author.name })
-  },
-  Book: {
-    author: book => find(authors, { name: book.author })
-  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
