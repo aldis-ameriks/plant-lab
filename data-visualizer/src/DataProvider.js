a823047c2a0a184db2e4f26f567a70cd3b43384e
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-10-14 17:13:54 +0300

    Use apollo for fetching sensor readings

--- data-visualizer/src/DataProvider.js
+++ data-visualizer/src/DataProvider.js
@@ -1,63 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import CircularProgress from '@material-ui/core/CircularProgress/CircularProgress';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
-class DataProvider extends React.Component {
-  static propTypes = {
-    render: PropTypes.func.isRequired
-  };
+const getFormattedTimeSinceLastReading = lastReadingTime => {
+  // TODO: use date-fns
+  const offsetInMilis = new Date() - lastReadingTime;
+  const minutes = Math.floor((offsetInMilis / (1000 * 60)) % 60);
+  const hours = Math.floor((offsetInMilis / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(offsetInMilis / (1000 * 60 * 60 * 24));
+  return `${days} days ${hours} hours ${minutes} minutes`;
+};
 
-  state = { data: null };
+const DataProvider = ({ render }) => (
+  <Query
+    pollInterval={30000}
+    query={gql`
+      {
+        readings(nodeid: 3, limit: 200) {
+          nodeid
+          time
+          temperature
+          moisture_precentage
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) {
+        return <CircularProgress style={{ display: 'block', margin: 'auto' }} />;
+      }
+      if (error) {
+        return <p>Error :(</p>;
+      }
 
-  constructor(props) {
-    super(props);
-    this.fetchData = this.fetchData.bind(this);
-  }
+      const { readings } = data;
+      const orderedData = readings.map(d => d.moisture_precentage).reverse();
+      const labels = readings.map(d => d.time).reverse();
+      const lastReadingTime = new Date(readings[0].time);
 
-  componentDidMount() {
-    this.intervalId = setInterval(this.fetchData, 5000);
-    this.fetchData();
-  }
+      const lastReadings = {
+        moisture: Number(readings[0].moisture_precentage).toFixed(2),
+        temperature: readings[0].temperature,
+        time: lastReadingTime.toLocaleString(),
+        timeSinceLastReading: getFormattedTimeSinceLastReading(lastReadingTime)
+      };
 
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
+      return render({ data: orderedData, labels, lastReadings });
+    }}
+  </Query>
+);
 
-  getFormattedTimeSinceLastReading(lastReadingTime) {
-    // TODO: use date-fns
-    const offsetInMilis = new Date() - lastReadingTime;
-    const minutes = Math.floor((offsetInMilis / (1000 * 60)) % 60);
-    const hours = Math.floor((offsetInMilis / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(offsetInMilis / (1000 * 60 * 60 * 24));
-    return `${days} days ${hours} hours ${minutes} minutes`;
-  }
-
-  async fetchData() {
-    const result = await axios.get('https://api.cleverhome.link/sensor?limit=200&nodeid=3');
-    this.setState({ data: result.data });
-  }
-
-  render() {
-    const { data } = this.state;
-    if (!data) {
-      return <CircularProgress style={{ display: 'block', margin: 'auto' }} />;
-    }
-
-    const orderedData = data.map(d => d.moisture_precentage).reverse();
-    const labels = data.map(d => d.time).reverse();
-    const lastReadingTime = new Date(data[0].time);
-
-    const lastReadings = {
-      moisture: Number(data[0].moisture_precentage).toFixed(2),
-      temperature: data[0].temperature,
-      time: lastReadingTime.toLocaleString(),
-      timeSinceLastReading: this.getFormattedTimeSinceLastReading(lastReadingTime)
-    };
-
-    const { render } = this.props;
-    return render({ data: orderedData, labels, lastReadings });
-  }
-}
+DataProvider.propTypes = {
+  render: PropTypes.func.isRequired
+};
 
 export default DataProvider;
--- data-visualizer/src/index.js
+++ data-visualizer/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
