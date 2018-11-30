afa57e94742f975f750c34bdf3d2b9dd03acb8d6
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-30 16:48:17 +0200

    Remove duplicate chart rendering plant moisture

--- sensor-visualizer/src/App.js
+++ sensor-visualizer/src/App.js
@@ -63,10 +63,6 @@ const App = () => (
                   </div>
                 </div>
               </div>
-
-              <div className="box">
-                <Chart data={moistures} labels={labels} label="Moisture" />
-              </div>
             </>
           )}
         />
