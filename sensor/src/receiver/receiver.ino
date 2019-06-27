602736b5a9d54b51c70a5f7020dd1fa0e1b0e4c5
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-06-27 10:17:44 +0300

    Remove raspberry pi relay and send data from arduino directly using ethernet shield

--- sensor/src/receiver/receiver.ino
+++ sensor/src/receiver/receiver.ino
@@ -3,6 +3,8 @@
 // any valid messages to the serial port felix@lowpowerlab.com
 
 #include <RFM12B.h>
+#include <SPI.h>
+#include <Ethernet.h>
 
 // You will need to initialize the radio by telling it what ID it has and what
 // network it's on The NodeID takes values from 1-127, 0 is reserved for sending
@@ -22,10 +24,34 @@ uint8_t KEY[] = "!Encrypted123%$£";
 
 // Need an instance of the Radio Module
 RFM12B radio;
+
+// assign a MAC address for the ethernet controller.
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 0, 177);
+IPAddress myDns(192, 168, 0, 1);
+EthernetClient client;
+
+char server[] = "api.plant.kataldi.com";
+
 void setup() {
+  Serial.begin(SERIAL_BAUD);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   radio.Initialize(NODEID, RF12_433MHZ, NETWORKID);
   radio.Encrypt(KEY); // comment this out to disable encryption
-  Serial.begin(SERIAL_BAUD);
+
+  // give the ethernet module time to boot up:
+  delay(1000);
+
+  Ethernet.begin(mac, ip, myDns);
+  // print the Ethernet board/shield's IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
@@ -34,6 +60,9 @@ void loop() {
       for (byte i = 0; i < *radio.DataLen; i++) {
         Serial.print((char)radio.Data[i]);
       }
+      Serial.println();
+
+      sendHttpRequestWithData(radio.Data);
 
       if (radio.ACKRequested()) {
         radio.SendACK();
@@ -41,6 +70,31 @@ void loop() {
     } else {
       Serial.print("BAD-CRC");
     }
-    Serial.println();
+  }
+}
+
+void sendHttpRequestWithData(String data) {
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
+  client.stop();
+
+  String postData = "{\"query\":\"mutation($input: String!) {saveReading(input: $input)}\",\"variables\":{\"input\":\"" + data + "\"}}";
+
+  if (client.connect(server, 80)) {
+    Serial.println("Sending request");
+
+    client.println("POST /graphql HTTP/1.1");
+    client.println("Host: api.plant.kataldi.com");
+    client.println("User-Agent: arduino-ethernet");
+    client.println("access-key: ---");
+    client.println("content-type: application/json");
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
+
+  } else {
+    Serial.println("connection failed");
   }
 }
--- sensor/src/relay/readme.md
+++
@@ -1,4 +0,0 @@
-# Relay
-Receives sensor readings from Arduino through serial and sends them to influxdb.
-
-In the future, might consider getting rid of the relay and sending the data to influxdb via api directly from Arduino (or using Raspberry PI as the rf receiver directly).
