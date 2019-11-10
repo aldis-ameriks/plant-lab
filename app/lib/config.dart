48f88e4b877ebe95c05812119c5011bd4e316cfb
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-11-10 14:47:26 +0200

    Update graphql endpoint

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -1,3 +1,2 @@
-final String HOST = 'localhost';
-final String GRAPHQL_ENDPOINT = 'http://$HOST:4000/graphql';
-final String SUBSCRIPTION_ENDPOINT = 'ws://$HOST:4000/subscriptions';
+final String GRAPHQL_ENDPOINT = 'https://api.plant.kataldi.com/graphql';
+final String SUBSCRIPTION_ENDPOINT = 'ws://api.plant.kataldi.com/subscriptions';
