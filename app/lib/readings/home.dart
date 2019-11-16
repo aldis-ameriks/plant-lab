4f7b0e8af08325881e2e8f60570bd9ca98ac4b70
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-11-16 09:44:16 +0200

    Avoid using stateful widget for home screen

--- app/lib/readings/home.dart
+++ app/lib/readings/home.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:planty/readings/sensor_card.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
