ad12aa06c9d3cb23795c5e6a8e58b1dc3a1a014f
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-11-16 19:00:41 +0200

    Add initial plant data rendering in a list

--- app/lib/plants/plants.dart
+++ app/lib/plants/plants.dart
@@ -5,8 +5,28 @@ class Plants extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PlantsQuery(builder: (result) {
-      print(result['plants'][0]['description']);
-      return Text(result['plants'][0]['name']);
+      dynamic plants = result['plants'];
+      return ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: plants.length,
+          separatorBuilder: (BuildContext context, int index) => const Divider(),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+                height: 50,
+                child: Row(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/plant.jpg'),
+                      width: 70,
+                      height: 70,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(plants[index]['name']),
+                    )
+                  ],
+                ));
+          });
     });
   }
 }
