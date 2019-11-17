b1fc076e514256f437c7af1dc45c3173d0188046
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-11-17 15:29:17 +0200

    Add poc sensor settings

--- app/lib/sensors/sensor_settings.dart
+++ app/lib/sensors/sensor_settings.dart
@@ -8,11 +8,56 @@ class SensorSettings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> entries = [
+      _buildEntry('Name', 'Plant Monitoring Sensor'),
+      _buildEntry('Plant', 'Rubber Tree'),
+      _buildEntry('Location', 'Current location for sensor'),
+      _buildEntry('Firmware', 'v1.0'),
+      _buildEntry('About', ''),
+      _buildEntry('Troubleshoot', ''),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Sensor settings'),
       ),
-      body: Text('Sensor settings placeholder'),
+      body: Container(
+        color: Colors.grey[300],
+        child: Container(
+          margin: EdgeInsets.only(top: 10),
+          color: Colors.white,
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 12, bottom: 12, left: 24),
+                child: entries[index],
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildEntry(text, value) {
+    return Flex(
+      direction: Axis.horizontal,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(text),
+        Row(
+          children: <Widget>[
+            Container(
+              width: 180,
+              child: Text(value, overflow: TextOverflow.ellipsis, textAlign: TextAlign.right),
+            ),
+            Icon(Icons.chevron_right, color: Colors.grey[600])
+          ],
+        ),
+      ],
     );
   }
 }
