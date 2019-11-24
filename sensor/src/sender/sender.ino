1bc98bdfedf998e3b9d817fff7674bcc59ea0a02
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-11-24 19:22:26 +0200

    Update sensor node to support board v1.0

--- sensor/src/sender/sender.ino
+++ sensor/src/sender/sender.ino
@@ -1,6 +1,5 @@
 #include <RFM69.h>
 #include <RFM69_ATC.h>
-#include <I2CSoilMoistureSensor.h>
 #include <LowPower.h>
 #include <Wire.h>
 #include <avr/sleep.h>
@@ -8,7 +7,7 @@
 
 #define SERIAL_BAUD 115200
 
-#define NODEID      2
+#define NODEID      1
 #define NETWORKID   100
 #define GATEWAYID   1
 #define FREQUENCY     RF69_433MHZ
@@ -32,62 +31,56 @@
   RFM69 radio;
 #endif
 
-int MOISTURE_DRY = 350;
-int MOISTURE_WET = 700;
-I2CSoilMoistureSensor sensor;
+#define WITH_REGULATOR
+
+// Calibrated values
+int MOISTURE_MIN = 240;
+int MOISTURE_MAX = 515;
 
 // For measuring battery voltage
-float R1 = 1000000.0; // R1 (1M)
-float R2 = 265000.0;  // R2 (270K)
+float R1 = 10000000.0; // R1 (10M)
+float R2 = 1000000.0;  // R2 (1M)
 float INTERNAL_AREF = 1.1;
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; // Wait for serial port to connect
   }
   radio.initialize(FREQUENCY, NODEID, NETWORKID);
 
   #ifdef IS_RFM69HW_HCW
-    radio.setHighPower(); //must include this only for RFM69HW/HCW!
+    radio.setHighPower(); // Must include this only for RFM69HW/HCW!
   #endif
 
   radio.encrypt(ENCRYPTKEY);
   radio.sleep();
   Serial.println("Radio module initialized");
 
-  // Init sensor
-  Serial.println("Initializing sensor");
-  delay(2000);    // give some time to boot up
-  Wire.begin();
-  sensor.begin(); // reset sensor
-  delay(1000);    // give some time to boot up
-  Serial.print("I2C Soil Moisture Sensor Address: ");
-  Serial.println(sensor.getAddress(), HEX);
-  Serial.print("Sensor Firmware version: ");
-  Serial.println(sensor.getVersion(), HEX);
-  Serial.println("Sensor initialized");
+  // Init light sensor
+  Serial.println("Initializing light sensor");
+  initializeLightSensor();
 
-  // Init analog ref to internal voltage (1.1v)
-  analogReference(INTERNAL);
+  // pinMode(13, OUTPUT); // SCK pin LED, flashes when interfacing with RFM69
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  pinMode(7, OUTPUT); // Powers capacitance sensor
+  pinMode(8, OUTPUT); // Powers temperature sensor
 }
 
 void loop() {
-  while (sensor.isBusy()) {
-    Serial.println("Sensor is busy");
-    delay(50);
-  }
-
-  Serial.println("Reading sensor values");
-  int moisture = sensor.getCapacitance();
-  float moisturePercentage = (1 - (MOISTURE_WET - moisture) / (MOISTURE_WET - (float)MOISTURE_DRY)) *100;
-  int temperature = sensor.getTemperature() / (float)10;
-//   unsigned int light = sensor.getLight(true);
-  unsigned int light = 0; // light sensor is covered on the rugged version of sensor
-  Serial.println("Putting sensor to sleep");
-  sensor.sleep();
-  Serial.println("Sensor sleeping");
   float batteryVoltage = readBatteryVoltage();
+  float operatingVoltage = batteryVoltage;
+  #ifdef WITH_REGULATOR
+    operatingVoltage = batteryVoltage > 3.31 ? 3.31 : batteryVoltage;
+  #endif
+
+  int moisture = readMoisture();
+  float moisturePercentage = (1 - (MOISTURE_MAX - moisture) / (MOISTURE_MAX - (float)MOISTURE_MIN)) * 100;
+
+  float temperature = readTemperature(operatingVoltage);
+  int light = readLight();
 
   String payload = "";
   payload += NODEID;
@@ -96,9 +89,9 @@ void loop() {
   payload += ";";
   payload += (String)moisturePercentage;
   payload += ";";
-  payload += MOISTURE_DRY;
+  payload += MOISTURE_MIN;
   payload += ";";
-  payload += MOISTURE_WET;
+  payload += MOISTURE_MAX;
   payload += ";";
   payload += temperature;
   payload += ";";
@@ -121,7 +114,13 @@ void sendData(String payload) {
   if (REQUEST_ACK) {
     if (waitForAck()) {
       Serial.print("ACK RECEIVED");
+      digitalWrite(6, HIGH);
+      delay(500);
+      digitalWrite(6, LOW);
     } else {
+      digitalWrite(5, HIGH);
+      delay(500);
+      digitalWrite(5, LOW);
       Serial.print("ACK NOT RECEIVED");
     }
   }
@@ -143,15 +142,96 @@ void enterSleep() {
   // 30 minutes = 60x30 = 1800s
   // 1800 s / 8 s = 225
   unsigned int sleepCounter;
-  for (sleepCounter = 225; sleepCounter > 0; sleepCounter--) {
+  for (sleepCounter = 1; sleepCounter > 0; sleepCounter--) {
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   }
 }
 
 float readBatteryVoltage() {
-  analogRead(0);
+  analogReference(INTERNAL);
+  delay(100);
+  analogRead(0); // Discard first reading after ADC init
   delay(50);
   int rawReading = analogRead(0);
-  float batteryVoltage = (rawReading/(float)1023) * INTERNAL_AREF * ((R1+R2)/R2);
+  float batteryVoltage = (rawReading / (float)1023) * INTERNAL_AREF * ((R1 + R2) / R2);
+  analogReference(DEFAULT);
+  delay(100);
   return batteryVoltage;
+}
+
+int readMoisture() {
+  digitalWrite(7, HIGH);
+  delay(100);
+  analogRead(A1); // Discard first reading after ADC init
+  delay(50);
+  int sampleCount = 10;
+  int sampleSum = 0.0;
+
+  for (int sample = 0; sample < sampleCount; sample++) {
+    int capacitance = analogRead(A1);
+    delay(20);
+    sampleSum += capacitance;
+  }
+
+  digitalWrite(7, LOW);
+  return sampleSum / (float) sampleCount;
+}
+
+float readTemperature(float operatingVoltage) {
+  digitalWrite(8, HIGH);
+  delay(100);
+  analogRead(A2); // Discard first reading after ADC init
+  delay(50);
+  float voltageOffset = 0.5; // Voltage offset at 0 degrees
+  float temperatureCoefficient = 0.01; // Temperature coefficient
+  int sampleCount = 1;
+  float sampleSum = 0.0;
+  float temperature;
+
+  for (int sample = 0; sample < sampleCount; sample++) {
+    temperature = ((float)analogRead(A2) * operatingVoltage / 1023.0);
+    temperature = (temperature - voltageOffset) / temperatureCoefficient;
+    delay(20);
+    sampleSum += temperature;
+  }
+  digitalWrite(8, LOW);
+  return sampleSum / (float)sampleCount;
+}
+
+int readLight() {
+
+  int sampleCount = 5;
+  float sampleSum = 0.0;
+
+  for (int sample = 0; sample < sampleCount; sample++) {
+    float fLux;
+    Wire.beginTransmission(0x44);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    delay(50);
+
+    Wire.requestFrom(0x44, 2);
+    uint16_t iData;
+    uint8_t  iBuff[2];
+    while (Wire.available()) {
+      Wire.readBytes(iBuff, 2);
+      iData = (iBuff[0] << 8) | iBuff[1];
+      uint16_t fraction = iData & 0x0FFF;
+      uint16_t exponent = (iData & 0xF000) >> 12;
+      fLux = fraction * (0.01 * pow(2, exponent));
+    }
+    sampleSum += fLux;
+  }
+
+  return sampleSum / (float)sampleCount;
+}
+
+void initializeLightSensor() {
+  Wire.begin();
+  Wire.beginTransmission(0x44);
+  // Turn on the sensor
+  Wire.write(0x01);
+  Wire.write(0xCE);
+  Wire.write(0x10);
+  Wire.endTransmission();
 }
