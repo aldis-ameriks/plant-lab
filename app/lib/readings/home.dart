b969b6f251d09967cc7e56cf920baffb95366e45
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-12-07 19:42:35 +0200

    Use custom scroll view with slivers in home screen

--- app/lib/readings/home.dart
+++ app/lib/readings/home.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:planty/readings/sensor_card.dart';
-import 'package:planty/sensors/new_sensor.dart';
 import 'package:planty/sensors/queries.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Sensors'),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.add_circle),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NewSensor()),
-                  );
-                }),
+    return Scaffold(body: UserSensorsQuery(
+      builder: (result) {
+        dynamic userSensors = result['sensors'];
+
+        return CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('Devices', style: Theme.of(context).textTheme.title),
+              ),
+            ),
+            SliverGrid(
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 1.5,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(margin: EdgeInsets.only(top: 10), child: SensorCard(sensor: userSensors[index]));
+                },
+                childCount: userSensors.length,
+              ),
+            )
           ],
-        ),
-        body: UserSensorsQuery(
-          builder: (result) {
-            dynamic userSensors = result['sensors'];
-            return Wrap(children: userSensors.map<Widget>((sensor) => SensorCard(sensor: sensor)).toList());
-          },
-        ));
+        );
+      },
+    ));
   }
 }
