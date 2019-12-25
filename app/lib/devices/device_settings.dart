27d82b66b55420fb5e442c71d1aada84b4660524
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-12-25 16:08:55 +0200

    Restructure device settings

--- app/lib/devices/device_settings.dart
+++ app/lib/devices/device_settings.dart
@@ -11,56 +11,54 @@ class SensorSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Device settings'),
-      ),
-      body: Container(
-        color: Colors.grey[300],
-        child: DeviceSettingsQuery(
-          deviceId: deviceId,
-          builder: (result) {
-            dynamic device = result['device'];
-
-            List<Widget> entries = [
-              _buildEntry('Device ID', device['id'], readonly: true),
-              _buildEntry('Name', device['name']),
-              _buildEntry('Room', device['room']),
-              _buildEntry('Firmware', device['firmware'], readonly: true),
-            ];
-
-            return Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  color: Colors.white,
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.all(10),
-                    itemCount: entries.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 12, bottom: 12, left: 24),
-                        child: entries[index],
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxScrolled) => [
+          SliverAppBar(
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('Device $deviceId', style: Theme.of(context).textTheme.title),
+              ),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.settings),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SensorSettings(deviceId: deviceId)),
                       );
-                    },
-                    separatorBuilder: (BuildContext context, int index) => const Divider(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    width: 500,
-                    child: RaisedButton(
-                      padding: EdgeInsets.all(12),
-                      onPressed: () {},
-                      color: Colors.red[700],
-                      child: Text("Remove", style: TextStyle(color: Colors.white)),
+                    }),
+              ]),
+        ],
+        body: Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          child: DeviceSettingsQuery(
+            deviceId: deviceId,
+            builder: (result) {
+              dynamic device = result['device'];
+
+              return Column(
+                children: <Widget>[
+                  _buildEntry('Device ID', device['id'], readonly: true),
+                  _buildEntry('Name', device['name']),
+                  _buildEntry('Room', device['room']),
+                  _buildEntry('Firmware', device['firmware'], readonly: true),
+                  Divider(),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Container(
+                      width: 500,
+                      child: RaisedButton(
+                        padding: EdgeInsets.all(12),
+                        onPressed: () {},
+                        color: Colors.red[700],
+                        child: Text("Remove", style: TextStyle(color: Colors.white)),
+                      ),
                     ),
-                  ),
-                )
-              ],
-            );
-          },
+                  )
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
@@ -68,22 +66,30 @@ class SensorSettings extends StatelessWidget {
 
   _buildEntry(text, value, {bool readonly: false}) {
     value ??= '';
-    return Flex(
-      direction: Axis.horizontal,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
       children: <Widget>[
-        Text(text),
-        Row(
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: Container(
-                  width: 180,
-                  child: Text(value, overflow: TextOverflow.ellipsis, textAlign: TextAlign.right),
-                )),
-            Opacity(opacity: readonly ? 0 : 1, child: Icon(Icons.chevron_right, color: Colors.grey[600]))
-          ],
-        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Flex(
+            direction: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(text),
+              Row(
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Container(
+                        width: 180,
+                        child: Text(value, overflow: TextOverflow.ellipsis, textAlign: TextAlign.right),
+                      )),
+                  Opacity(opacity: readonly ? 0 : 1, child: Icon(Icons.chevron_right, color: Colors.grey[600]))
+                ],
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
