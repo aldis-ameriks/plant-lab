72d77b31c530695b65317df8ccd5382159de2aec
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-12-29 23:45:51 +0200

    Extract entry widget

---
+++ app/lib/device-settings/entry.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Entry extends StatefulWidget {
+  const Entry({@required this.text, @required this.value, this.readonly: false, this.onSubmit, this.loading = false});
+
+  final String text;
+  final String value;
+  final bool readonly;
+  final Function onSubmit;
+  final bool loading;
+
+  @override
+  EntryState createState() => EntryState();
+}
+
+class EntryState extends State<Entry> {
+  TextEditingController myController;
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    myController = new TextEditingController(text: widget.value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Divider(),
+        GestureDetector(
+          onTap: () {
+            if (widget.readonly) {
+              return;
+            }
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                      actions: <Widget>[
+                        if (widget.loading) RefreshProgressIndicator(),
+                        FlatButton(
+                          child: Text('Cancel'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: Text('Submit'),
+                          onPressed: () {
+                            print(myController.text);
+                            widget.onSubmit(myController.text);
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ],
+                      title: Text('Update ${widget.text}'),
+                      content: Form(
+                        key: GlobalKey(),
+                        child: Padding(padding: EdgeInsets.all(8.0), child: TextFormField(controller: myController)),
+                      ));
+                });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(widget.text),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    if (widget.loading)
+                      Container(
+                        margin: EdgeInsets.only(right: 6),
+                        height: 12,
+                        width: 12,
+                        child: CircularProgressIndicator(strokeWidth: 2),
+                      ),
+                    Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: Container(
+                          constraints: BoxConstraints(maxWidth: 180),
+                          child: Text(myController.text ?? '',
+                              overflow: TextOverflow.ellipsis, textAlign: TextAlign.right),
+                        )),
+                    Opacity(opacity: widget.readonly ? 0 : 1, child: Icon(Icons.chevron_right, color: Colors.grey[600]))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
