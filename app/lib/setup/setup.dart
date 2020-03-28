087cf36873804fb5068f28eab26b9aec5658c0b3
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-03-28 15:23:08 +0200

    Allow setting access key by scanning QR code

--- app/lib/setup/setup.dart
+++ app/lib/setup/setup.dart
@@ -1,5 +1,7 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import '../user_state_provider.dart';
@@ -11,6 +13,7 @@ class Setup extends StatefulWidget {
 
 class _SetupState extends State<Setup> {
   TextEditingController myController;
+  String barcodeError;
 
   @override
   void initState() {
@@ -21,50 +24,71 @@ class _SetupState extends State<Setup> {
   @override
   Widget build(BuildContext context) {
     return Consumer<UserState>(
-      builder: (context, userState, _) => Scaffold(
-          body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(child: Text("Missing access key"), margin: EdgeInsets.only(bottom: 20)),
-            RaisedButton(
-              child: const Text('Scan QR code'),
-              onPressed: () {},
-            ),
-            RaisedButton(
-              child: const Text('Enter access key'),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                          actions: <Widget>[
-                            FlatButton(
-                              child: Text('Cancel', style: TextStyle(color: Colors.black)),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            FlatButton(
-                              child: Text('Submit', style: TextStyle(color: Colors.black)),
-                              onPressed: () {
-                                userState.setAccessKey(myController.text);
-                                Navigator.of(context).pop();
-                              },
-                            )
-                          ],
-                          title: Text('Enter access key'),
-                          content: Form(
-                            key: GlobalKey(),
-                            child:
-                                Padding(padding: EdgeInsets.all(8.0), child: TextFormField(controller: myController)),
-                          ));
-                    });
-              },
-            ),
-          ],
-        ),
-      )),
+      builder: (context, userState, _) {
+        Future scan() async {
+          try {
+            String barcode = await BarcodeScanner.scan();
+            userState.setAccessKey(barcode);
+          } on PlatformException catch (e) {
+            if (e.code == BarcodeScanner.CameraAccessDenied) {
+              setState(() {
+                this.barcodeError = 'Missing camera permission.';
+              });
+            } else {
+              setState(() => this.barcodeError = 'Unknown error: $e');
+            }
+          } catch (e) {
+            setState(() => this.barcodeError = 'Unknown error: $e');
+          }
+        }
+
+        return Scaffold(
+            body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(child: Text("Missing access key"), margin: EdgeInsets.only(bottom: 20)),
+              barcodeError != null
+                  ? Container(child: Text(barcodeError), margin: EdgeInsets.only(bottom: 20))
+                  : Container(),
+              RaisedButton(
+                child: const Text('Scan QR code'),
+                onPressed: scan,
+              ),
+              Container(child: Text("or")),
+              RaisedButton(
+                child: const Text('Enter access key'),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text('Cancel', style: TextStyle(color: Colors.black)),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                FlatButton(
+                                  child: Text('Submit', style: TextStyle(color: Colors.black)),
+                                  onPressed: () {
+                                    userState.setAccessKey(myController.text);
+                                    Navigator.of(context).pop();
+                                  },
+                                )
+                              ],
+                              title: Text('Enter access key'),
+                              content: Form(
+                                key: GlobalKey(),
+                                child: Padding(
+                                    padding: EdgeInsets.all(8.0), child: TextFormField(controller: myController)),
+                              )));
+                },
+              ),
+            ],
+          ),
+        ));
+      },
     );
   }
 }
