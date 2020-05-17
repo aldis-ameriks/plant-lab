7a8a87e2e227decf258fb53cb51b85b5c1563be4
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-05-17 12:18:04 +0300

    Add missing primary key for notifications

--- server/migrations/20200510003011_notifications.ts
+++ server/migrations/20200510003011_notifications.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<any> {
   await knex.raw(`
     CREATE TYPE notification_type AS enum('low_moisture', 'low_battery');
     CREATE TABLE notifications (
-        id serial8,
+        id serial8 PRIMARY KEY,
         user_id int2 REFERENCES users(id) ON DELETE RESTRICT ON UPDATE CASCADE NOT NULL,
         device_id int2 REFERENCES devices(id) ON DELETE RESTRICT ON UPDATE CASCADE,
         created_at timestamptz NOT NULL DEFAULT current_timestamp,
