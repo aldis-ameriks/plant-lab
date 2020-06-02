0852355de9810d293a2201a3479b85ad6e8ba162
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-06-02 21:10:17 +0300

    Add eslint ignore for any

--- server/src/common/test-helpers/mockClassMethods.ts
+++ server/src/common/test-helpers/mockClassMethods.ts
@@ -3,5 +3,6 @@ export function mockClassMethods<T extends Function>(clazz: T) {
   return methods.reduce((acc, curr) => {
     acc[curr] = jest.fn();
     return acc;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   }, {}) as any;
 }
