4e6f607438b109068fbe4690576316813c3c16ec
Aldis Ameriks <aldis.ameriks@gmail.com>
2022-12-26 19:32:52 +0200

    Fix tests

--- apps/api/src/modules/errors/routes.test.ts
+++ apps/api/src/modules/errors/routes.test.ts
@@ -17,7 +17,7 @@ beforeEach(() => {
 describe('error', () => {
   test('posting error works', async () => {
     const count = await knex('errors')
-      .where('source', 'frontend')
+      .where('source', 'web')
       .count()
       .first()
       .then((res) => (res ? +res.count : 0))
@@ -32,12 +32,12 @@ describe('error', () => {
     expect(response.body).toBe('OK')
     expect(response.statusCode).toBe(200)
 
-    const errors = await knex<ErrorEntity>('errors').where('source', 'frontend')
+    const errors = await knex<ErrorEntity>('errors').where('source', 'web')
     expect(errors.length).toBe(2)
 
     expect(errors[0].content).toEqual({ foo: 'bar' })
-    expect(errors[0].source).toBe('frontend')
+    expect(errors[0].source).toBe('web')
     expect(errors[1].content).toEqual({ foo: 'bar2' })
-    expect(errors[1].source).toBe('frontend')
+    expect(errors[1].source).toBe('web')
   })
 })
