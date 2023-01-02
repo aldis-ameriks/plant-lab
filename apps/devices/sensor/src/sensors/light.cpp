b45ab064ddb4a1dabb1319b6999732509dc46c3e
Aldis Ameriks <aldis.ameriks@gmail.com>
2023-01-02 20:46:25 +0200

    Fix light sensor

--- apps/devices/sensor/src/sensors/light.cpp
+++ apps/devices/sensor/src/sensors/light.cpp
@@ -10,6 +10,11 @@ void LightSensor::init() {
 };
 
 uint32_t LightSensor::read() {
+    while (!lightMeter.measurementReady(true)) {
+        // wait until measurement is ready
+    }
+    
     float lux = lightMeter.readLightLevel();
+    lightMeter.configure(BH1750::ONE_TIME_HIGH_RES_MODE);
     return (uint32_t)lux;
 }
