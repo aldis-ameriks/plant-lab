1e7ac49899e4b93c366e88baf2ec1d44b2161492
Aldis Ameriks <aldis.ameriks@gmail.com>
2024-03-19 01:04:27 +0200

    Fix test coverage

--- apps/api/src/modules/readings/helpers/getReadingQuery.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from '../../../helpers/context'
-import { readings } from '../../../helpers/schema'
-
-export function getReadingQuery(db: Context['db']) {
-  return db.select().from(readings).$dynamic()
-}
