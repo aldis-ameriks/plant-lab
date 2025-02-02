2924afc76e3d6020f41bb96ef3408b3621fc7460
Aldis Ameriks <aldis.ameriks@gmail.com>
2025-02-02 17:53:13 +0200

    Remove request helper in favor of built-in fetch

--- apps/api/src/modules/errors/helpers/sendTelegram.ts
+++ apps/api/src/modules/errors/helpers/sendTelegram.ts
@@ -1,17 +1,18 @@
 import { type Context } from '../../../helpers/context.ts'
-import { request } from '../../../helpers/request.ts'
 
 export function sendTelegram(context: Pick<Context, 'config'>, text: string) {
   const { config } = context
   if (!config.telegram.accessKey || !config.telegram.receiver) {
     return
   }
-  return request(`https://api.telegram.org/bot${config.telegram.accessKey}/sendMessage`, {
+
+  return fetch(`https://api.telegram.org/bot${config.telegram.accessKey}/sendMessage`, {
     method: 'POST',
     body: JSON.stringify({
       chat_id: config.telegram.receiver,
       text
     }),
+    signal: AbortSignal.timeout(30000),
     headers: {
       'content-type': 'application/json'
     }
